83ac5aa8007dc69500d0154eaafb615325a7eaf3
jacob moya <jemoya1994@Gmail.com>
2026-08-30 23:56:32 -0700

    more tinkering, more reading, more learning

--- git-go/main.go
+++ git-go/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
+	// "path/filepath"
 )
 
 func main() {
+
 	// saving args
 	args := os.Args[1:]
-	fmt.Println(filepath.Base(os.Args[0]))
-	fmt.Println(args[0])
-	fmt.Println(args[1])
+
+	if len(args) > 0 {
+		fmt.Println(args[0])
+	}
 
 }

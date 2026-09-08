42d1864551d5283d77482054b003fa41826651e8
jacob moya <jemoya1994@Gmail.com>
2026-09-07 23:55:59 -0700

    restructuring app for git-go & learning branched separately

--- git-go/main.go
+++ git-go/main.go
@@ -2,24 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	// saving args
-	args := os.Args[1:]
-	osStuff, err := os.ReadFile("progress.md")
-
-	if err != nil {
-		fmt.Println("This is the error %w",err)
-	}
-
-	fmt.Printf("osStuff: %v\n", osStuff)
-
-	if len(args) != 1 {
-		fmt.Println("Review: Multiple Args Present")
-		return 
-	}
-
-	printArgs(args[0])
+	fmt.Println("Main git-go") 
 }

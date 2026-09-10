788b894cea3adabdb7b08ef1ea03743b36e5e1ff
jacob moya <jemoya1994@Gmail.com>
2026-09-09 21:22:30 -0700

    working with some structs, laaahve it lol

--- git-go/main.go
+++ git-go/main.go
@@ -6,6 +6,18 @@ import (
 )
 
 func main() {
+
+	// Addition of Person struct
+	paul := Person{
+		Name: "paul",
+		Hair: "red",
+		Age:  24,
+	}
+
+	paul.birthday()
+
+	paul.displayPerson()
+
 	// slice expression to just graba the items after the executable path
 	args := os.Args[1:]
 	displayArgs(args)
---
+++ git-go/structs.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Hair string
+	Age  int
+}
+
+func (p Person) displayPerson() {
+	fmt.Println("Name: ", p.Name)
+	fmt.Println("Hair: ", p.Hair)
+	fmt.Println("Age: ", p.Age)
+}
+
+func (p *Person) birthday() {
+	p.Age ++
+}

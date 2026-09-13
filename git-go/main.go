1b23be7267debc5804b2b0ed177fefc7d24907a9
jacob moya <jemoya1994@Gmail.com>
2026-09-13 14:57:25 -0700

    Keep struct practice on learning/structs

--- git-go/main.go
+++ git-go/main.go
@@ -6,28 +6,9 @@ import (
 )
 
 func main() {
-
-	// Addition of Person struct
-	person := Person{
-		Name: "Paul",
-		Hair: "red",
-		Age:  24,
-	}
-
-	person.birthday()
-
-	person.displayPerson()
-
 	// slice expression to just graba the items after the executable path
 	args := os.Args[1:]
 	displayArgs(args)
-
-	changeName(&person, "jacob")
-	changeHair(&person, "periwinkle")
-	changeAge(&person, 32)
-
-	fmt.Println(person)
-
 }
 
 func displayArgs(args []string) {

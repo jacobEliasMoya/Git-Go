4eed4a395da1e3905072a5ca39a5f29808d40daa
jacob moya <jemoya1994@Gmail.com>
2026-09-15 23:53:25 -0700

    working on conditional logic to determine number of args before moving into building full command

--- git-go/main.go
+++ git-go/main.go
@@ -10,26 +10,31 @@ func main() {
 	// slice expression to just graba the items after the executable path
 	args := os.Args[1:]
 	hasArgs := returnArgs(args)
+	gitType := args[0]
+
+	// gitScope := args[1]
+	// gitMessage := args[2]
 
 	if hasArgs {
-		fmt.Println("Has Args:")
-		commitType(args[0])
+		fmt.Println("Has Args: ", len(args))
+		commitType(gitType)
 	} else {
 		fmt.Println("No Args")
 	}
 }
 
 func returnArgs(args []string) bool {
-
 	if len(args) == 0 {
 		return false
 	}
-
 	return true
 }
 
 func commitType(arg string) (string, bool) {
+
 	// list of items that should be available
+	// "fix", "feat", "refactor", "docs", "test" for now
+
 	argTypes := []string{"fix", "feat", "refactor", "docs", "test"}
 
 	if slices.Contains(argTypes, arg) {
